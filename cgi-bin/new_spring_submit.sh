b5363247b1e23d355772a1e06b994aa5af250bdd
Sam Wolock <swolock@gmail.com>
2018-02-02 13:07:06 -0500

    Fix new subplot online version

--- cgi-bin/new_spring_submit.sh
+++ cgi-bin/new_spring_submit.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo $1 > logsub
-cgi-bin/spring_from_selection_execute.py $1/params.pickle > logsub2 2>&1 &
+cgi-bin/spring_from_selection_execute.online.py $1/params.pickle > logsub2 2>&1 &
